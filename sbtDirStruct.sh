725be02714fbf2e00e06f3d226f26fae2a5a645f
Rutvik Patel <heyrutvik@gmail.com>
2017-06-15 12:31:32 +0530

    autogenerate sbt directory structure

---
+++ sbtDirStruct.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p src/{main,test}/{java,resources,scala}
+mkdir lib project target
+
+# create an initial build.sbt file
+echo "name := \"$1\"
+version := \"$2\"
+scalaVersion := \"$3\"" > build.sbt
+
+# add eclipse plugin
+echo 'addSbtPlugin("com.typesafe.sbteclipse" %% "sbteclipse-plugin" % "5.1.0")' > project/plugins.sbt
